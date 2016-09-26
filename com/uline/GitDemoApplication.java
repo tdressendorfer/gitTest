38094b60001e9986c522d91fb451c389a6aa3333
tdressendorfer <tdressendorfer@uline.com>
2016-09-26 13:34:32 -0500

    Update GitDemoApplication.java

--- com/uline/GitDemoApplication.java
+++ com/uline/GitDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitDemoApplication {
 
 	public static void main(String[] args) {
+		// Some Change
 		SpringApplication.run(GitDemoApplication.class, args);
 	}
 }
